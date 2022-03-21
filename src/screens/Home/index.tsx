a18aaf87cb2bdc39345f5d5f8d5175038b4336d2
azeez1776 <aboud3am81@gmail.com>
2022-03-21 18:53:11 +0300

    smh finished header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import UserAvatar from 'react-native-user-avatar';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Home = () => {
   return (
-    <View>
-        <View>
-            <View>
-            <Text style={{color:'black'}}><UserAvatar size={30} style={{borderRadius:5}} imageStyle={{borderRadius:5}} name="Avishay Bar" src="https://res.cloudinary.com/deex1bwvl/image/upload/v1647738498/Bluescope/alex-suprun-ZHvM3XIOHoE-unsplash_ptlaxf.jpg" />Hi, Louis</Text>
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <View >
+            <Text style={{color:'black'}}><UserAvatar size={35} style={{}} imageStyle={{}} name="Avishay Bar" src="https://res.cloudinary.com/deex1bwvl/image/upload/v1647738498/Bluescope/alex-suprun-ZHvM3XIOHoE-unsplash_ptlaxf.jpg" />Hi, Louis</Text>
             </View>
+            <Icon name="filter" size={30} color="#BDBDBD" />
         </View>
         <View>
             {/* Products */}
@@ -19,4 +21,17 @@ const Home = () => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        width:'100%',
+        alignItems:'center'
+    },
+    header:{
+        flex:0.4,
+        flexDirection:'row',
+        justifyContent:'space-between',
+        width:'90%',
+        marginTop:20
+    }
+})
