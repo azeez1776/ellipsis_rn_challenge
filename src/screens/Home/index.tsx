ad225eab64af4c38c7fc3f1537ae4e8f1b436b14
azeez1776 <aboud3am81@gmail.com>
2022-03-21 18:23:48 +0300

    added avator

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import UserAvatar from 'react-native-user-avatar';
 
 const Home = () => {
   return (
     <View>
         <View>
             <View>
-                
+            <Text style={{color:'black'}}><UserAvatar size={30} style={{borderRadius:5}} imageStyle={{borderRadius:5}} name="Avishay Bar" src="https://res.cloudinary.com/deex1bwvl/image/upload/v1647738498/Bluescope/alex-suprun-ZHvM3XIOHoE-unsplash_ptlaxf.jpg" />Hi, Louis</Text>
             </View>
         </View>
         <View>
